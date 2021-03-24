f4432a1044793a8b352e91ba0325219cfd06a6e0
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2021-03-24 17:39:12 +0300

    add view all and filtered cards

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ buttonCart.addEventListener('click', openModal);
 
 modalCart.addEventListener('click', function (event) {
 	const target = event.target;
-	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) {
+	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) { //проверка на наличие класса
 		closeModal()
 	}
 });
@@ -36,10 +36,10 @@ modalCart.addEventListener('click', function (event) {
 (function() {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-	for (let i = 0; i < scrollLinks.length; i++) {
-		scrollLinks[i].addEventListener('click', function(event) {
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', function(event) {
 			event.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start'
@@ -48,3 +48,85 @@ modalCart.addEventListener('click', function (event) {
 	}
 })()
 
+// goods (view all)
+
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function () {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Ошибочка вышла: ' + result.status
+	}
+	return await result.json();
+};
+
+const createCard = function ({ label, name, img, description, id, price }) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+
+	//Деструктуризация(переменных)ES6, можно записать так или сразу в функцию
+	//const { label, name, img, description, id, price } = objCard;
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${label ? 
+				`<span class="label">${label}</span>` :
+				''}
+			<img src="db/${img}" alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>
+	`;
+	return card;
+};
+
+const renderCards = function(data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
+};
+
+more.addEventListener('click', function(event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
+
+
+// Фильтруем карточки
+
+const filterCards = function(field, value) {
+	getGoods()
+		.then(function (data) {
+			const filteredGoods = data.filter(function(good) {
+				return good[field] === value
+			});
+			return filteredGoods;
+		})
+		.then(renderCards);
+};
+
+navigationLink.forEach(function(link) {
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field, value);
+	})
+});
+
+
+
+// ДЗ пункт 1
+
+const navigationLinkAll = document.querySelector('.navigation-link-all');
+
+navigationLinkAll.addEventListener('click', function(event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
